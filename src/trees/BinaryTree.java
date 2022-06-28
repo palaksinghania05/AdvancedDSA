35a0502724def95aae506778d40d5ce34d510a88
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-06-28 19:29:18 +0530

    added trees code

---
+++ src/trees/BinaryTree.java
@@ -0,0 +1,48 @@
+package trees;
+
+public class BinaryTree {
+    public TreeNode root;
+
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data; //can be any generic type
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
+    }
+
+    public void preOrder(TreeNode root){
+        //base case
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+        bt.preOrder(bt.root);
+    }
+}
---
+++ src/trees/RecursiveInsertion.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 24-01-2022
+ */
+
+package trees;
+
+
+public class RecursiveInsertion {
+    public void insert(int value) {
+
+    }
+}
